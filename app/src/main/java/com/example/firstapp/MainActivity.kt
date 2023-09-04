8505855eb9c30ea7ef6df9a49a845e463a2f96c1
baymaks <s2071662@ed.ac.uk>
2023-09-04 21:00:23 +0100

    a list and an animation

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,17 +16,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstapp.ui.theme.FirstappTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +43,7 @@ class MainActivity : ComponentActivity() {
             FirstappTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -75,7 +84,13 @@ fun MessageCard(msg: Message) {
                 // Add a horizontal space between the image and the column
                 Spacer(modifier = Modifier.width(8.dp))
 
-                Column {
+                var isExpanded by remember { mutableStateOf(false) }
+                // surfaceColor will be updated gradually from one color to the other
+                val surfaceColor by animateColorAsState(
+                    if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+                )
+
+                Column(modifier = Modifier.clickable{ isExpanded = !isExpanded }) {
                     Text(
                         text = msg.author,
                         color = MaterialTheme.colorScheme.secondary,
@@ -83,16 +98,44 @@ fun MessageCard(msg: Message) {
                     )
                     // Add a vertical space between the author and message texts
                     Spacer(modifier = Modifier.height(4.dp))
-                    Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    // Surface basically allows to group elements together and add a common design to them
+                    Surface(
+                        shape = MaterialTheme.shapes.medium,
+                        shadowElevation = 1.dp,
+                        // surfaceColor color will be changing gradually from primary to surface
+                        color = surfaceColor,
+                        // animateContentSize will change the Surface size gradually
+                        modifier = Modifier.animateContentSize().padding(1.dp)
+                    ) {
                         Text(
                             text = msg.body,
                             modifier = Modifier.padding(all = 4.dp),
+                            // If the message is expanded, we display all its content
+                            // otherwise we only display the first line
+                            maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                             style = MaterialTheme.typography.bodyMedium
                         )
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    FirstappTheme() {
+        Conversation(SampleData.conversationSample)
     }
 }
 
